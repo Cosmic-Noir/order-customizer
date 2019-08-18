6dda2c4b123138e68fe4510448f027d2f2dc787b
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-18 16:17:17 -0600

    Changed Cart and Form Component

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Form from "./Form/Form";
+import Cart from "./Cart/Cart";
 
 // Normalizes string as a slug - a string that is safe to use
 // in both URLs and html attributes
-import slugify from 'slugify';
+import slugify from "slugify";
 
-import './App.css';
+import "./App.css";
 
 // This object will allow us to
 // easily convert numbers into US dollar values
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD'
+const USCurrencyFormat = new Intl.NumberFormat("en-US", {
+  style: "currency",
+  currency: "USD"
 });
 
 class App extends Component {
   state = {
     selected: {
       Processor: {
-        name: '17th Generation Intel Core HB (7 Core with donut spare)',
+        name: "17th Generation Intel Core HB (7 Core with donut spare)",
         cost: 700
       },
-      'Operating System': {
-        name: 'Ubuntu Linux 16.04',
+      "Operating System": {
+        name: "Ubuntu Linux 16.04",
         cost: 200
       },
-      'Video Card': {
-        name: 'Toyota Corolla 1.5v',
+      "Video Card": {
+        name: "Toyota Corolla 1.5v",
         cost: 1150.98
       },
       Display: {
@@ -45,7 +47,7 @@ class App extends Component {
 
   render() {
     const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
+      const featureHash = feature + "-" + idx;
       const options = this.props.features[feature].map(item => {
         const itemHash = slugify(JSON.stringify(item));
         return (
@@ -76,7 +78,7 @@ class App extends Component {
     });
 
     const summary = Object.keys(this.state.selected).map((feature, idx) => {
-      const featureHash = feature + '-' + idx;
+      const featureHash = feature + "-" + idx;
       const selectedOption = this.state.selected[feature];
 
       return (
@@ -101,20 +103,8 @@ class App extends Component {
           <h1>ELF Computing | Laptops</h1>
         </header>
         <main>
-          <form className="main__form">
-            <h2>Customize your laptop</h2>
-            {features}
-          </form>
-          <section className="main__summary">
-            <h2>Your cart</h2>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Total</div>
-              <div className="summary__total__value">
-                {USCurrencyFormat.format(total)}
-              </div>
-            </div>
-          </section>
+          <Form />
+          <Cart />
         </main>
       </div>
     );
