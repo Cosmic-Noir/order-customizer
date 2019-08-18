9da78c01afb0fe2d448a176e6c95c6f228a41d08
Kristen Doucette <cosmicnoir90@gmail.com>
2019-08-18 17:21:38 -0600

    Fix object being passed to Form component

--- src/App.js
+++ src/App.js
@@ -46,36 +46,7 @@ class App extends Component {
   };
 
   render() {
-    const features = Object.keys(this.props.features).map((feature, idx) => {
-      const featureHash = feature + "-" + idx;
-      const options = this.props.features[feature].map(item => {
-        const itemHash = slugify(JSON.stringify(item));
-        return (
-          <div key={itemHash} className="feature__item">
-            <input
-              type="radio"
-              id={itemHash}
-              className="feature__option"
-              name={slugify(feature)}
-              checked={item.name === this.state.selected[feature].name}
-              onChange={e => this.updateFeature(feature, item)}
-            />
-            <label htmlFor={itemHash} className="feature__label">
-              {item.name} ({USCurrencyFormat.format(item.cost)})
-            </label>
-          </div>
-        );
-      });
-
-      return (
-        <fieldset className="feature" key={featureHash}>
-          <legend className="feature__name">
-            <h3>{feature}</h3>
-          </legend>
-          {options}
-        </fieldset>
-      );
-    });
+    console.log(this.props);
 
     return (
       <div className="App">
@@ -83,8 +54,8 @@ class App extends Component {
           <h1>ELF Computing | Laptops</h1>
         </header>
         <main>
-          <Form features={this.props} />
-          <Cart selected={this.state.selected} />
+          <Form features={this.props.features} />
+          {/* <Cart selected={this.state.selected} /> */}
         </main>
       </div>
     );
